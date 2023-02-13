9bf06c044a29efc39edec199cb6408e3fdec7081
Ezequiel <erubio07@gmail.com>
2023-02-13 09:39:45 -0300

    node

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,7 +1,25 @@
+const { clear } = require("console");
 const process = require("process");
 const commands = require("./commands/index.js");
 
+const print = (ouput) => {
+  process.stdout.write(ouput);
+  process.stdout.write("\nprompt > ");
+};
+
 function bash() {
+  process.stdout.write("prompt > ");
+  process.stdin.on("data", (data) => {
+    let args = data.toString().trim().split(" ");
+    //console.log(args);
+    let cmd = args.shift();
+    //console.log(cmd);
+    if (!commands[cmd]) {
+      print(`command not found: ${cmd}`);
+    } else {
+      commands[cmd](print, args);
+    }
+  });
 }
 
 bash();
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -1,21 +1,63 @@
 const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
+const { error } = require("console");
 
-function pwd() {}
+function pwd(print) {
+  print(process.cwd());
+}
 
-function date() {}
+function date(print) {
+  print(Date());
+}
 
-function echo() {}
+function echo(print, args) {
+  print(args);
+}
 
-function ls() {}
+function ls(print) {
+  fs.readdir(".", (err, files) => {
+    //console.log(files);
+    if (err) throw err;
+    else print(files.join(" "));
+  });
+}
 
-function cat() {}
+function cat(print, args) {
+  fs.readFile(args, "utf-8", (err, data) => {
+    if (err) throw err;
+    else print(data);
+  });
+}
 
-function head() {}
+function head(print, args) {
+  fs.readFile(args, "utf-8", (err, data) => {
+    if (err) throw err;
+    else print(data.split("\n").at(0));
+  });
+}
 
-function tail() {}
+function tail(print, args) {
+  fs.readFile(args, "utf-8", (err, data) => {
+    if (err) throw err;
+    else print(data.split("\n").at(-1).trim());
+  });
+}
 
-function curl() {}
+function curl(print, args) {
+  utils.request(args, (err, response) => {
+    if (err) throw err;
+    else print(response);
+  });
+}
 
-module.exports = {};
+module.exports = {
+  pwd,
+  date,
+  echo,
+  ls,
+  cat,
+  head,
+  tail,
+  curl,
+};
