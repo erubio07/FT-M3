7a7a1ee1d9c0f934c1e145b37500bdda23a9bc26
Ezequiel <erubio07@gmail.com>
2023-02-16 13:58:23 -0300

    express

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -21,26 +21,29 @@ server.post("/posts", (req, res) => {
       contents,
     };
     publications.push(newPublications);
-    res.status(200).json(newPublications);
+    res.status(200).json(publications);
   }
 });
 
 server.get("/posts", (req, res) => {
-  const { author, title } = req.query;
-  if (!author || !title) {
-    req
-      .status(404)
-      .json({
-        error:
-          "No existe ninguna publicación con dicho título y autor indicado",
-      });
-  } else {
-    let result = publications.find(
-      (pub) => pub.author == author && pub.title == title
-    );
-    res.status(200).send(result);
+  const {author, title} = req.query;
+  if(author && title){
+    const result = publications.filter(pub => pub.author == author && pub.title == title)
+    res.status(200).json(result);
+  }else {
+    res.status(400).json({"error": "No existe ninguna publicación con dicho título y autor indicado"});
   }
 });
+
+server.get('/posts/:author', (req, res) =>{
+ const {author} = req.params;
+ if(author){
+  const searchPublications = publications.filter(p => p.author == author)
+  res.status(200).json(searchPublications);
+ }else{
+  res.status(404).json({"error": "No existe ninguna publicación del autor indicado"})
+ }
+})
 
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
 module.exports = { publications, server };
